a1e0279099868f8b8b41566c8a1eeb842cd47259
Thomas Tumiel <ttumiel@gmail.com>
2023-09-17 21:51:36 +0100

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="chat2func",
-    version="0.1.0",
+    version="0.1.1",
     description="Automatically creating functions that LLMs can use.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
