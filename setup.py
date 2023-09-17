435261fa178eff27d423ee2fb64ad291f4bde331
Thomas Tumiel <ttumiel@gmail.com>
2023-09-17 20:47:39 +0100

    add desc

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup
 setup(
     name="autocoder",
     version="0.1.0",
-    description="Automatically creating functions that LLMs can use..",
+    description="Automatically creating functions that LLMs can use.",
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
     author="Thomas Tumiel",
     packages=["autocoder"],
     license="MIT",
